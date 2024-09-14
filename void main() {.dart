e65287fa2dd65e418c045506be596701af430798
YasinSaydut478 <145253147+YasinSaydut478@users.noreply.github.com>
2024-09-14 17:36:57 +0300

    yasin ödev
    kodluyoruz

---
+++ void main() {.dart
@@ -0,0 +1,34 @@
+void main() {
+
+  int yas = 25;
+  String isim = "Yasin";
+  double boy = 1.73;
+  bool evliMi = false;
+  dynamic dinamikDeger = "Merhaba";
+  String karakter = 'A';
+
+  print(yas);
+  print(isim);
+  print(boy);
+  print(evliMi);
+  print(dinamikDeger);
+  print(karakter);
+
+  int yasimCamelCase = 25;
+  int yasim_snake_case = 25;
+  int YasimPascalCase = 25;
+
+  print(yasimCamelCase);
+  print(yasim_snake_case);
+  print(YasimPascalCase);
+
+  String adim = "Yasin";
+  String soyadim = "Saydut";
+  int yasin = 18;  
+  bool resitMi = yasin >= 18; 
+
+  print("Adım: $adim");
+  print("Soyadım: $soyadim");
+  print("Yaşım: $yasin"); 
+  print("Reşit miyim? $resitMi");
+}
